9c838207c8e34b12ded03f917fe96a9f5343eec4
juanuranidev <juanuranidev@gmail.com>
2023-10-04 23:59:36 -0300

    Deleted some language services

--- src/services/language.services.js
+++ src/services/language.services.js
@@ -1,41 +1,9 @@
-import languageRepository from "../repositories/language.repository.js";
+import codeRepository from "../repositories/code.repository.js";
 
 class LanguageService {
-  async findAll() {
-    try {
-      return await languageRepository.findAll();
-    } catch (error) {
-      throw new Error(error);
-    }
-  }
-
   async findById(id) {
     try {
-      return await languageRepository.findById(id);
-    } catch (error) {
-      throw new Error(error);
-    }
-  }
-
-  async create(language) {
-    try {
-      return await languageRepository.create(language);
-    } catch (error) {
-      throw new Error(error);
-    }
-  }
-
-  async update(id, language) {
-    try {
-      return await languageRepository.update(id, language, { new: true });
-    } catch (error) {
-      throw new Error(error);
-    }
-  }
-
-  async delete(id) {
-    try {
-      return await languageRepository.delete(id);
+      return await codeRepository.findById(id);
     } catch (error) {
       throw new Error(error);
     }
