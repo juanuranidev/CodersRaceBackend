315d4bc800a69035baab54229cc8621822de2629
juanuranidev <juanuranidev@gmail.com>
2023-11-26 00:59:18 -0300

    Added a new user route and modified user controller

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { CodeRoutes } from "./code/routes";
+import { UserRoutes } from "./user/routes";
 import { LanguageRoutes } from "./language/routes";
 
 export class AppRoutes {
@@ -8,6 +9,7 @@ export class AppRoutes {
 
     router.use("/api/language", LanguageRoutes.routes);
     router.use("/api/code", CodeRoutes.routes);
+    router.use("/api/user", UserRoutes.routes);
 
     return router;
   }
--- src/presentation/user/controller.ts
+++ src/presentation/user/controller.ts
@@ -13,26 +13,22 @@ export class UserController {
       return res.status(400).json({ error: error });
     }
 
-    const userCreated = await new CreateUser(this.userRepository).execute(
-      userDto!
-    );
-
-    res.status(201).json(userCreated);
+    await new CreateUser(this.userRepository)
+      .execute(userDto!)
+      .then((user) => res.json(user))
+      .catch((error) => res.status(400).json({ error }));
   };
 
   public getUserById = async (req: Request, res: Response) => {
     const id = +req.params.id;
-    console.log({ id });
 
     if (!id) {
       res.status(400).json({ error: "Id not found" });
     }
 
-    const user = await new GetUserById(this.userRepository)
+    await new GetUserById(this.userRepository)
       .execute(Number(id))
       .then((user) => res.json(user))
       .catch((error) => res.status(400).json({ error }));
-
-    res.status(200).json(user);
   };
 }
