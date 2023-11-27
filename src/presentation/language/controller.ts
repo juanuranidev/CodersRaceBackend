7ab8d69aee2e5ec30b422bd1f8fb43d2b010b0d0
juanuranidev <juanuranidev@gmail.com>
2023-11-27 00:32:13 -0300

    Modified language routes and controller

--- src/presentation/language/controller.ts
+++ src/presentation/language/controller.ts
@@ -1,14 +1,14 @@
-import { PrismaClient } from "@prisma/client";
-
+import { GetLanguages } from "../../domain/use-cases";
 import { Request, Response } from "express";
 import { LanguageRepository } from "../../domain/repositories";
-import { GetLanguages } from "../../domain/use-cases";
 
 export class LanguageController {
   constructor(private readonly languageRepository: LanguageRepository) {}
 
   public getLanguages = async (req: Request, res: Response) => {
-    const languages = await new GetLanguages(this.languageRepository).execute();
-    res.status(200).json(languages);
+    new GetLanguages(this.languageRepository)
+      .execute()
+      .then((languages) => res.status(200).json(languages))
+      .catch((error) => res.status(400).json(error));
   };
 }
--- src/presentation/language/routes.ts
+++ src/presentation/language/routes.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
-import { LanguageRepositoryImpl } from "../../infraestructure/repositories/language.repository.impl";
 import { LanguageController } from "./controller";
-
+import { LanguageRepositoryImpl } from "../../infraestructure/repositories";
 export class LanguageRoutes {
   static get routes(): Router {
     const router = Router();
