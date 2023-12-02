9c74d550e2f8f6c15a83cba870cc4c240c3eec98
juanuranidev <juanuranidev@gmail.com>
2023-12-01 23:56:08 -0300

    Modified user entity

--- src/domain/entities/user.entity.ts
+++ src/domain/entities/user.entity.ts
@@ -4,11 +4,12 @@ export class UserEntity {
     public image: string,
     public githubUsername: string,
     public githubId: string,
-    public id: number
+    public id: number,
+    public averageCpm?: number
   ) {}
 
   public static fromObject(object: { [key: string]: any }): UserEntity {
-    const { name, image, githubUsername, githubId, id } = object;
+    const { name, image, githubUsername, githubId, id, averageCpm } = object;
 
     if (!name) throw "name is required";
     if (!image) throw "image is required";
@@ -16,6 +17,13 @@ export class UserEntity {
     if (!githubId) throw "githubID is required";
     if (!id) throw "id is required";
 
-    return new UserEntity(name, image, githubUsername, githubId, id);
+    return new UserEntity(
+      name,
+      image,
+      githubUsername,
+      githubId,
+      id,
+      averageCpm
+    );
   }
 }
